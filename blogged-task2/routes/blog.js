2ba57902c0e9e12b71fdef503c6b351240e3cbcb
Swayam Atal <atalswayam28@gmail.com>
2026-03-31 01:39:53 +0530

    Added comments feature in the blog

--- blogged-task2/routes/blog.js
+++ blogged-task2/routes/blog.js
@@ -3,6 +3,7 @@ const Blog = require("../models/blog");
 const router = Router();
 const multer = require("multer");
 const path=require("path");
+const Comment = require("../models/comment");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -29,10 +30,25 @@ router.post("/add-blog", upload.single("cover"),async (req, res) => {
 });
 router.get("/:id",async(req,res)=>{
     const blog=await Blog.findById(req.params.id).populate("createdBy");
-
+    const comments=await Comment.find({
+        blogId:req.params.id,
+    }).populate("createdBy");
     return res.render("blog",{
         user:req.user,
         blog,
+        comments,
     });
+});
+
+
+router.post("/comments/:blogId",async(req,res)=>{
+    await Comment.create({
+    content: req.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
 })
+
+
 module.exports = router;
